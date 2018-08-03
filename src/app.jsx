0fef4e7faeba2b4fd8efaa24ba2203cbf0b9f94d
Michael Kovich <kovich@michaelkovich.com>
2018-08-03 11:37:19 -0400

    Initial Electron Forge commit.

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 style={{ fontSize: 18, color: 'green' }}>Welcome to Electron!</h2>
+      </div>
+    );
+  }
+}
